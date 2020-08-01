39926f11c95d6b816daba0883c2f2bffb97cada1
Takeda Eiji <you@example.com>
2020-08-01 16:07:10 +0900

    上端点より下端点が大きい閉区間を作ることはできないためのロジックをコンストラクタに追加

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -22,7 +22,7 @@ describe('ClosedRangeテスト', () => {
 
   describe('整数閉区間オブジェクトは下端点上端点のフィールドを持つこと', () => {
     it('下端点に2が設定されていること', () => {
-      rangeObj = new ClosedRange(2, 0);
+      rangeObj = new ClosedRange(2, 3);
       assert(rangeObj.lowerEndpoint === 2);
     });
     it('上端点に1が設定されていること', () => {
@@ -50,9 +50,13 @@ describe('ClosedRangeテスト', () => {
       rangeObj = new ClosedRange("hoge", 1.1);
       assert(rangeObj.upperEndpoint === undefined);
     });
-    it('上端点より下端点が大きい閉区間を作ることはできないこと', () => {
+    it('上端点より下端点が大きい閉区間を作ることはできないこと(正常系)', () => {
       rangeObj = new ClosedRange(3, 1);
       assert(rangeObj.lowerEndpoint === undefined && rangeObj.upperEndpoint === undefined);
     });
+    it('上端点より下端点が大きい閉区間を作ることはできないこと(異常系)', () => {
+      rangeObj = new ClosedRange("hoge", 1);
+      assert(rangeObj.lowerEndpoint === undefined && rangeObj.upperEndpoint === undefined);
+    });    
   });
 });
