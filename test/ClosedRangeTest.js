007b514ce7295707124899cd122914e085926bd5
Takeda Eiji <you@example.com>
2020-08-01 14:55:37 +0900

    フィールドの整数値チェックを追加

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -3,10 +3,10 @@ const assert = require('assert');
 const ClosedRange = require('../lib/closedRange');
 
 describe('ClosedRangeテスト', () => {
-  let rangeObj; 
+  let rangeObj;
 
   beforeEach(() => {
-    
+
   });
 
   describe('整数閉区間を示すクラス（あるいは構造体）', () => {
@@ -18,11 +18,11 @@ describe('ClosedRangeテスト', () => {
     it('整数閉区間を示すクラス（あるいは構造体）の型であること', () => {
       //TODO: あとで型判定いれたいな
       rangeObj = new ClosedRange(1, 2);
-      assert(rangeObj instanceof(ClosedRange));
+      assert(rangeObj instanceof (ClosedRange));
     });
   });
-    
-    describe('整数閉区間を示すクラス（あるいは構造体）', () => {
+
+  describe('整数閉区間オブジェクトは下端点上端点のフィールドを持つこと', () => {
     it('上端点に1が設定されていること', () => {
       rangeObj = new ClosedRange(0, 1);
       assert(rangeObj.upperEndpoint === 1);
@@ -35,5 +35,9 @@ describe('ClosedRangeテスト', () => {
       rangeObj = new ClosedRange(2, 0);
       assert(rangeObj.lowerEndpoint === 2);
     });
+    it('下端点に整数値以外が設定されないこと', () => {
+      rangeObj = new ClosedRange("hoge", "hoge");
+      assert(rangeObj.lowerEndpoint === undefined);
+    });    
   });
 });
