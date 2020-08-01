ca8b37e50a24fd1a6165b597ef9407673b6f2f42
RentaroMinami <minami.rentaro@gmail.com>
2020-08-01 15:53:17 +0900

    上端点のテストを追加、テスト項目の名称を修正

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -11,18 +11,20 @@ describe('ClosedRangeテスト', () => {
 
   describe('整数閉区間を示すクラス（あるいは構造体）', () => {
     it('整数閉区間を示すクラス（あるいは構造体）がnullでないこと', () => {
-      //TODO: あとで型判定いれたいな
       rangeObj = new ClosedRange(1, 2);
       assert(rangeObj !== null);
     });
     it('整数閉区間を示すクラス（あるいは構造体）の型であること', () => {
-      //TODO: あとで型判定いれたいな
       rangeObj = new ClosedRange(1, 2);
       assert(rangeObj instanceof (ClosedRange));
     });
   });
 
   describe('整数閉区間オブジェクトは下端点上端点のフィールドを持つこと', () => {
+    it('下端点に2が設定されていること', () => {
+      rangeObj = new ClosedRange(2, 0);
+      assert(rangeObj.lowerEndpoint === 2);
+    });
     it('上端点に1が設定されていること', () => {
       rangeObj = new ClosedRange(0, 1);
       assert(rangeObj.upperEndpoint === 1);
@@ -31,13 +33,14 @@ describe('ClosedRangeテスト', () => {
       rangeObj = new ClosedRange(0, 2);
       assert(rangeObj.upperEndpoint === 2);
     });
-    it('下端点に2が設定されていること', () => {
-      rangeObj = new ClosedRange(2, 0);
-      assert(rangeObj.lowerEndpoint === 2);
-    });
-    it('下端点に整数値以外が設定されないこと', () => {
+    
+    it('下端点に文字列が設定されないこと', () => {
       rangeObj = new ClosedRange("hoge", "hoge");
       assert(rangeObj.lowerEndpoint === undefined);
-    });    
+    });
+    it('上端点に文字列が設定されないこと', () => {
+      rangeObj = new ClosedRange("hoge", "hoge");
+      assert(rangeObj.upperEndpoint === undefined);
+    });
   });
 });
