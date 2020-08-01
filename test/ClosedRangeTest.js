0f81b46eac960b6fad7e8e130df501fd08394037
Takeda Eiji <you@example.com>
2020-08-01 13:27:52 +0900

    test作成

---
+++ test/ClosedRangeTest.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const ClosedRange = require('../lib/closedRange');
+
+describe('ClosedRangeテスト', () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = new ClosedRange();
+  });
+});
