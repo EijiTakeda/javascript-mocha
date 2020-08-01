f3fe005b803dd1f679cc9fc9df4c4a68cd9f4acf
RentaroMinami <minami.rentaro@gmail.com>
2020-08-01 14:34:57 +0900

    コンストラクタの引数の順序を変更し、ClosedRange型判定のテストを追加(DOING)

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -12,19 +12,24 @@ describe('ClosedRangeテスト', () => {
   describe('#isGeneratedInstance()', () => {
     it('インスタンスが生成されていること', () => {
       //TODO: あとで型判定いれたいな
-      rangeObj = new ClosedRange(1);
+      rangeObj = new ClosedRange(1, 2);
       assert(rangeObj !== null);
     });
+    it('ClosedRange型のインスタンスであること', () => {
+      //TODO: あとで型判定いれたいな
+      rangeObj = new ClosedRange(1, 2);
+      assert(rangeObj === hogehoge);
+    });
     it('上端点に1が設定されていること', () => {
-      rangeObj = new ClosedRange(1);
+      rangeObj = new ClosedRange(0, 1);
       assert(rangeObj.upperEndpoint === 1);
     });
     it('上端点に2が設定されていること', () => {
-      rangeObj = new ClosedRange(2);
+      rangeObj = new ClosedRange(0, 2);
       assert(rangeObj.upperEndpoint === 2);
     });
     it('下端点に2が設定されていること', () => {
-      rangeObj = new ClosedRange(2, 2);
+      rangeObj = new ClosedRange(2, 0);
       assert(rangeObj.lowerEndpoint === 2);
     });
   });
