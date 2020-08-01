bee327ffcdc4ec1cca2fd730a3bf1f42d30a99a3
Usachan <yagamilaitoaspect@yahoo.co.jp>
2020-08-01 16:19:06 +0900

    コンストラクタのロジック修正、端点より下端点が大きい閉区間を作ることはできないことリファクタリング

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -1,62 +1,68 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const ClosedRange = require('../lib/closedRange');
+const ClosedRange = require("../lib/closedRange");
 
-describe('ClosedRangeテスト', () => {
+describe("ClosedRangeテスト", () => {
   let rangeObj;
 
-  beforeEach(() => {
+  beforeEach(() => {});
 
-  });
-
-  describe('整数閉区間を示すクラス（あるいは構造体）', () => {
-    it('整数閉区間を示すクラス（あるいは構造体）がnullでないこと', () => {
+  describe("整数閉区間を示すクラス（あるいは構造体）", () => {
+    it("整数閉区間を示すクラス（あるいは構造体）がnullでないこと", () => {
       rangeObj = new ClosedRange(1, 2);
       assert(rangeObj !== null);
     });
-    it('整数閉区間を示すクラス（あるいは構造体）の型であること', () => {
+    it("整数閉区間を示すクラス（あるいは構造体）の型であること", () => {
       rangeObj = new ClosedRange(1, 2);
-      assert(rangeObj instanceof (ClosedRange));
+      assert(rangeObj instanceof ClosedRange);
     });
   });
 
-  describe('整数閉区間オブジェクトは下端点上端点のフィールドを持つこと', () => {
-    it('下端点に2が設定されていること', () => {
+  describe("整数閉区間オブジェクトは下端点上端点のフィールドを持つこと", () => {
+    it("下端点に2が設定されていること", () => {
       rangeObj = new ClosedRange(2, 3);
       assert(rangeObj.lowerEndpoint === 2);
     });
-    it('上端点に1が設定されていること', () => {
+    it("上端点に1が設定されていること", () => {
       rangeObj = new ClosedRange(0, 1);
       assert(rangeObj.upperEndpoint === 1);
     });
-    it('上端点に2が設定されていること', () => {
+    it("上端点に2が設定されていること", () => {
       rangeObj = new ClosedRange(0, 2);
       assert(rangeObj.upperEndpoint === 2);
     });
-    
-    it('下端点に文字列が設定されないこと', () => {
+
+    it("下端点に文字列が設定されないこと", () => {
       rangeObj = new ClosedRange("hoge", "hoge");
       assert(rangeObj.lowerEndpoint === undefined);
     });
-    it('上端点に文字列が設定されないこと', () => {
+    it("上端点に文字列が設定されないこと", () => {
       rangeObj = new ClosedRange("hoge", "hoge");
       assert(rangeObj.upperEndpoint === undefined);
     });
-    it('下端点に小数値が設定されないこと', () => {
+    it("下端点に小数値が設定されないこと", () => {
       rangeObj = new ClosedRange(1.1, "hoge");
       assert(rangeObj.lowerEndpoint === undefined);
     });
-    it('上端点に小数値が設定されないこと', () => {
+    it("上端点に小数値が設定されないこと", () => {
       rangeObj = new ClosedRange("hoge", 1.1);
       assert(rangeObj.upperEndpoint === undefined);
     });
-    it('上端点より下端点が大きい閉区間を作ることはできないこと(正常系)', () => {
-      rangeObj = new ClosedRange(3, 1);
-      assert(rangeObj.lowerEndpoint === undefined && rangeObj.upperEndpoint === undefined);
-    });
-    it('上端点より下端点が大きい閉区間を作ることはできないこと(異常系)', () => {
-      rangeObj = new ClosedRange("hoge", 1);
-      assert(rangeObj.lowerEndpoint === undefined && rangeObj.upperEndpoint === undefined);
+    describe("上端点より下端点が大きい閉区間を作ることはできないこと", () => {
+      it("上端点より下端点が大きい閉区間を作ることはできないこと(正常系)", () => {
+        rangeObj = new ClosedRange(3, 1);
+        assert(
+          rangeObj.lowerEndpoint === undefined &&
+            rangeObj.upperEndpoint === undefined
+        );
+      });
+      it("上端点より下端点が大きい閉区間を作ることはできないこと(異常系)", () => {
+        rangeObj = new ClosedRange("hoge", 1);
+        assert(
+          rangeObj.lowerEndpoint === undefined &&
+            rangeObj.upperEndpoint === undefined
+        );
+      });
     });
   });
 });
