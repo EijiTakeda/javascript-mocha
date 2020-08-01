a69a41b13c52ef791aaecb403ce8a660ce6020db
Usachan <yagamilaitoaspect@yahoo.co.jp>
2020-08-01 16:00:31 +0900

    小数値の確認、上端点より下端点が大きい閉区間を作ることはできないことテスト作成

--- test/ClosedRangeTest.js
+++ test/ClosedRangeTest.js
@@ -42,5 +42,17 @@ describe('ClosedRangeテスト', () => {
       rangeObj = new ClosedRange("hoge", "hoge");
       assert(rangeObj.upperEndpoint === undefined);
     });
+    it('下端点に小数値が設定されないこと', () => {
+      rangeObj = new ClosedRange(1.1, "hoge");
+      assert(rangeObj.upperEndpoint === undefined);
+    });
+    it('上端点に小数値が設定されないこと', () => {
+      rangeObj = new ClosedRange("hoge", 1.1);
+      assert(rangeObj.upperEndpoint === undefined);
+    });
+    it('上端点より下端点が大きい閉区間を作ることはできないこと', () => {
+      rangeObj = new ClosedRange(3, 1);
+      assert(rangeObj.lowerEndpoint === undefined && rangeObj.upperEndpoint === undefined);
+    });
   });
 });
